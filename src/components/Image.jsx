c7edc84cceb936f25d0945dc5039b0de1b13daa7
a.haerdrich@gmail.com <a.haerdrich@gmail.com>
2022-12-05 15:02:44 +0100

    set static height

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,6 @@
 export default function Image({ data }) {
   return (
-    <main class="w-screen bg-white relative">
+    <main class="w-screen bg-white relative h-[390px]">
       <div class="imageContainer w-full relative bg-white">
         <img
           src={data.image_0.src}
